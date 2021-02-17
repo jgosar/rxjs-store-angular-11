dc3c65d3da0375cb98687cc80c78eb9eda69dc19
jgosar <36840705+jgosar@users.noreply.github.com>
2021-02-17 19:14:44 +0100

    Added test-app

---
+++ test-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { Store } from "rxjs-observable-store";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = 'test-app';
+}
